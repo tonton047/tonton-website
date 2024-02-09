ee933ae4559ad12a257a4a796adb6f9686321ff2
TONTON <119924040+tonton047@users.noreply.github.com>
2024-02-09 18:50:45 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,20 +4,12 @@ function handleButtonClick() {
   // 添加按钮点击后的样式
   contactButton.classList.add('cb1-clicked');
 
-  // 监听过渡结束事件
-  contactButton.addEventListener('transitionend', function handleTransitionEnd() {
-      // 移除过渡结束事件监听器
-      contactButton.removeEventListener('transitionend', handleTransitionEnd);
-
-      // 在过渡结束后打开发邮件的页面
-      openEmailWindow();
-      
-      // 移除按钮点击后的样式
-      contactButton.classList.remove('cb1-clicked');
-  });
+  // 在按钮点击时直接打开邮件窗口
+  openEmailWindow();
+  
+  // 移除按钮点击后的样式
+  contactButton.classList.remove('cb1-clicked');
 }
-
-/* 轻点 */
 
 function handleTouchStart() {
   const contactButton = document.getElementById('contactButton');
@@ -29,9 +21,10 @@ function handleTouchEnd() {
   contactButton.classList.remove('cb1-touched');
 }
 
-
-
 function openEmailWindow() {
   // 使用 window.open 打开发送邮件的窗口
   window.open('mailto:tong0738@outlook.com?subject=Subject&body=Body%20of%20the%20email');
 }
+
+
+/* 窗口小于720时 */
