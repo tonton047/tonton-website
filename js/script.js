0ca0dec2e90aa69b78a6f4099b0feee6e2da051b
TONTON <119924040+tonton047@users.noreply.github.com>
2024-02-10 12:49:35 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,30 +1,32 @@
 function handleButtonClick() {
   const contactButton = document.getElementById('contactButton');
-  
+
   // 添加按钮点击后的样式
   contactButton.classList.add('cb1-clicked');
 
-  // 在按钮点击时直接打开邮件窗口
+  // 在新窗口中打开邮件窗口
   openEmailWindow();
-  
-  // 移除按钮点击后的样式
-  contactButton.classList.remove('cb1-clicked');
 }
 
-function handleTouchStart() {
+function handleMouseDown() {
   const contactButton = document.getElementById('contactButton');
   contactButton.classList.add('cb1-touched');
 }
 
-function handleTouchEnd() {
+function handleMouseUp() {
   const contactButton = document.getElementById('contactButton');
-  contactButton.classList.remove('cb1-touched');
+  contactButton.classList.remove('cb1-touched', 'cb1-clicked');
 }
 
 function openEmailWindow() {
-  // 使用 window.open 打开发送邮件的窗口
-  window.open('mailto:tong0738@outlook.com?subject=Subject&body=Body%20of%20the%20email');
-}
+  // 使用 window.open 打开新窗口，并传递 mailto 链接
+  const emailAddress = 'tong0738@outlook.com';
+  const subject = 'Hello';
+  const body = '"Hi nice to meet you, Any question just let me know ;)"';
+  const mailtoLink = `mailto:${emailAddress}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
 
+  // 使用 window.open 打开发送邮件的窗口
+  window.open(mailtoLink, '_blank');
+}
 
 /* 窗口小于720时 */
