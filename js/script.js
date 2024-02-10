bfecf2d2fc2809d647cb243a2558ae2c4927ec4c
TONTON <119924040+tonton047@users.noreply.github.com>
2024-02-11 02:44:24 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// script.js
+
 function handleButtonClick() {
   const contactButton = document.getElementById('contactButton');
 
@@ -32,4 +34,89 @@ function openEmailWindow() {
   }
 }
 
-/* 窗口小于720时 */
+
+
+/* swiper */
+let swiperOptions = {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true
+  },
+  keyboard: true,
+  mousewheel: false,
+  
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false
+  }
+  
+};
+
+// 初始化Swiper
+let swiper = new Swiper(".swiper", swiperOptions);
+
+// 监听窗口大小变化事件
+window.addEventListener("resize", () => {
+  // 获取当前窗口宽度
+  let windowWidth = window.innerWidth;
+
+  // 根据窗口宽度判断是否启用 mousewheel
+  if (windowWidth < 720) {
+    swiper.mousewheel.enable();
+  } else {
+    swiper.mousewheel.disable();
+  }
+});
+
+// 初始时检查窗口宽度，以决定是否启用 mousewheel
+if (window.innerWidth < 720) {
+  swiper.mousewheel.enable();
+} else {
+  swiper.mousewheel.disable();
+}
+
+
+/* 侧边栏 */
+
+function toggleSidebar() {
+  const sidebar = document.getElementById('sidebar');
+  const entranceBox = document.querySelector('.entrancebox');
+  const imageWrappers = document.querySelectorAll('.image__wrapper');
+
+  // 切换 sidebar 和 entrancebox 的类
+  sidebar.classList.toggle('sidebar-open');
+  entranceBox.classList.toggle('sidebar-open');
+
+  // 切换 right 的值，从 0 到 -50vh，或者从 -50vh 到 0
+  sidebar.style.right = sidebar.style.right === '0px' ? '-60vh' : '0';
+
+  // 切换图像的模糊效果
+  imageWrappers.forEach(wrapper => {
+    // 检查是否已经应用了模糊效果
+    const isBlurred = wrapper.classList.contains('blur');
+
+    // 根据当前状态切换模糊效果和添加过渡类
+    wrapper.classList.toggle('blur', !isBlurred);
+    wrapper.classList.toggle('unblur', isBlurred);
+  });
+
+  // 切换 entrancebox 的 margin-right
+  entranceBox.style.marginRight = entranceBox.classList.contains('sidebar-open') ? '60vh' : '0';
+}
+
+
+
+
+/* menu-icon 动效 */
+/*————————————————————————————————————————————*/
+function toggleIcon() {
+  const icon = document.querySelector('.menu-icon');
+  icon.classList.toggle('open');
+}
+/*————————————————————————————————————————————*/
